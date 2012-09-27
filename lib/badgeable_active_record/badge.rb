3e0e9a1a7367ce9c8a3b26c9c15618a909c127ff
Justin Saul <justins@aplaceformom.com>
2012-09-27 16:08:26 -0700

    using personalized gem now

--- lib/badgeable_active_record/badge.rb
+++ lib/badgeable_active_record/badge.rb
@@ -1,7 +1,15 @@
 class Badge < ActiveRecord::Base
   has_many :badgings
   
+  def self.find_or_create_by_name(name)
+    Badge.where(:name => name).first || create(:name => name)
+  end
+  
   def icon
-    "/images/#{name.parameterize}.jpg"
+    if read_attribute(:icon)
+      read_attribute(:icon)
+    else
+      "/images/#{name.parameterize}.jpg"
+    end
   end
 end
